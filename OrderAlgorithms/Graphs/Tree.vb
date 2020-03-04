e292c3d9a5e6e20eaab6e618d84504c61705f0b7
dilearmo <dilearmo@hotmail.com>
2020-03-03 20:11:26 -0600

    archivos faltantes

---
+++ OrderAlgorithms/Graphs/Tree.vb
@@ -0,0 +1,13 @@
+﻿Public Class Tree
+
+    Private Property root As Node = Nothing
+    Private Property profucndidad As Integer
+    Private Property anchura As Integer
+
+    Public Function Add()
+        If (root Is Nothing) Then
+
+        End If
+    End Function
+
+End Class
